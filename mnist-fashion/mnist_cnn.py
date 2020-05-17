77c022181a10f13c159a9f16ad6e12157562be8d
Karthik Rao <karthik.rao543@gmail.com>
2020-05-16 23:27:44 -0400

    first commit

---
+++ mnist-fashion/mnist_cnn.py
@@ -0,0 +1,55 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(gpus[0], True)
+
+print(tf.__version__)
+
+fashion_mnist = tf.keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+train_images = train_images / 255.0
+
+test_images = test_images / 255.0
+
+train_labels = tf.keras.utils.to_categorical(train_labels)
+test_labels = tf.keras.utils.to_categorical(test_labels)
+
+print(train_labels[10:])
+
+train_images = train_images.reshape(60000, 28, 28, 1)
+test_images = test_images.reshape(10000, 28, 28, 1)
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(64, kernel_size=(3,3), activation='relu', input_shape=(28,28,1)),
+    tf.keras.layers.Conv2D(32, kernel_size=(3,3), activation='relu'),
+    tf.keras.layers.MaxPool2D(pool_size=(2,2)),
+    tf.keras.layers.Dropout(0.25),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
+
+model.summary()
+
+model.compile(optimizer='adam', 
+              loss='categorical_crossentropy', 
+              metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=10)
+
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+
+print('\nTest accuracy:', test_acc)
+
+probability_model = tf.keras.Sequential([model, 
+                                         tf.keras.layers.Softmax()])
+
+predictions = probability_model.predict(test_images)
+predictions[0]
+np.argmax(predictions[0])
